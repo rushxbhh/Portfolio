0fe5fb91a3c5f957b7ee14a149512a70688e2d08
Rushabh Singh Baghel <rushabhsingh6@gmail.com>
2026-04-23 23:59:56 +0530

    it wasnt working at props so fixed this shit

--- src/components/Panel/ProjectsPanel.jsx
+++ src/components/Panel/ProjectsPanel.jsx
@@ -229,7 +229,7 @@ export default function ProjectsPanel({ onClose }) {
             placeholder="Momento Screenshot Coming Soon"
             description="Real time chat application using WebSockets (STOMP), dockerized the backend and deployed it on AWS and deployed react frontend on Ngnix"
             tags={["Java", "Spring Boot", "React" , "Redis" , "Docker", "AWS (s3)"]}
-            onView={() => setActiveProject("momento")}
+            onView={() => setActiveProject(PROJECT_DATA.momento)}
           />
 
            <ProjectCard
@@ -237,7 +237,7 @@ export default function ProjectsPanel({ onClose }) {
             placeholder="JOB scheduler Screenshot Coming Soon"
             description="Distributed Job Scheduler using Springboot and Kafka on microservices architecture ensured idempotency accross nodes"
             tags={["Java", "Spring Boot", "Microservices" , "MYSQL" , "Docker", "Kafka"]}
-            onView={() => setActiveProject("jobScheduler")}
+            onView={() => setActiveProject(PROJECT_DATA.jobScheduler)}
           />
         </div>
       </motion.div>
